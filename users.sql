d476b5e8d6fb309d4c3ead7429bc9d73921936cb
BijaySwain <abc.bijay4@gmail.com>
2017-07-25 23:31:30 +0530

    SQL query file for the database
    Please directly import these two files via phpmyadmin of your wampserver.

---
+++ users.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 25, 2017 at 07:57 PM
+-- Server version: 5.6.17
+-- PHP Version: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `users`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE IF NOT EXISTS `admin` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `admin_name` varchar(20) NOT NULL,
+  `admin_pass` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  FULLTEXT KEY `admin_name` (`admin_name`),
+  FULLTEXT KEY `admin_name_2` (`admin_name`),
+  FULLTEXT KEY `admin_name_3` (`admin_name`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `admin_name`, `admin_pass`) VALUES
+(1, 'admin', '123456789');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(20) unsigned NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(40) NOT NULL,
+  `user_email` varchar(30) NOT NULL,
+  `user_pass` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_email` (`user_email`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `user_name`, `user_email`, `user_pass`) VALUES
+(2, 'droid', 'dro@gmail.com', '123'),
+(3, 'b', 'b@gmail.com', '123');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
